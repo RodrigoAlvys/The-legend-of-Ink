fbac6317d1602ce3a0148580f0ef373aa9c5c3fe
LuizArt9 <166402385+LuizArt9@users.noreply.github.com>
2026-06-05 13:05:06 -0300

    Sistema de dialogo
    adicionou o sistema de dialogo de npcs, e um dialogo simples para o npc vermelho

--- rp-gprojeto/jogador/Controles.gd
+++ rp-gprojeto/jogador/Controles.gd
@@ -7,6 +7,7 @@ var facing_direction = Vector2.DOWN
 
 @onready var raycast = $RayCast2D
 
+
 func _physics_process(delta):
 	var direction = Input.get_vector(
 		"ui_left",
@@ -26,14 +27,20 @@ func _physics_process(delta):
 	velocity = direction * speed
 	move_and_slide()
 
-	# Mantém o RayCast apontando para frente
 	raycast.target_position = facing_direction * 64
 
 
 func _input(event):
 	if event.is_action_pressed("interact"):
+
+		if DialogueUI.is_active():
+			DialogueUI.next()
+			return
+
+		raycast.force_raycast_update()
+
 		if raycast.is_colliding():
 			var obj = raycast.get_collider()
 
-			if obj.has_method("interact"):
+			if obj and obj.has_method("interact"):
 				obj.interact()
