19116a44264b6c86a737be73b6ef94ce719471e0
Rodrigo Alves <rodrigoalvys@gmail.com>
2026-06-14 21:50:42 -0300

    Adicionando pequenas funções e começando a fazer a "ficha" do jogador

--- rp-gprojeto/scripts/combat/BaseCharacter.gd
+++ rp-gprojeto/scripts/combat/BaseCharacter.gd
@@ -1,29 +1,39 @@
 #BaseCharacter
 # Classe abstrata
-extends CharacterBody2D
-
+extends Node2D
 class_name BaseCharacter
 
-var _name:Array[String] = []
-var fullname:String:
+const BASE_ATTRIBUTES:Array[String] = [
+	"STR",
+	"DEX",
+	"CON",
+	"ESP"
+]
+var _name:Array[String] = [""]
+@export var fullname:String:
 	get: return " ".join(_name)
 	set(new):
-		self._name = new.strip_edges().capitalize().split()
+		self._name = new.strip_edges().left(30).capitalize().split()
 var first_name:String:
 	get: return _name[0] if not self._name.is_empty() else ""
 	set(new):
-		self._name[0] = new.strip_edges().capitalize().split()[0]
+		if _name.is_empty():
+			self._name = [""]
+		new = new.strip_edges().capitalize().split()[0]
+		var current_surname:String = " ".join(_name.slice(1))
+		self.fullname = new + " " + current_surname
 var surname:String = "":
 	get: return " ".join(self._name.slice(1))
 	set(new):
-		var new_array:Array[String] = new.strip_edges().capitalize().split()
 		if _name.is_empty():
 			self._name = [""]
-		var newer:Array[String] = [self._name[0]]
-		self._name = newer + new_array
+		new = new.strip_edges().capitalize()
+		var current_first_name:String = _name[0]
+		self.fullname = current_first_name + " " + new
+		
 # F = Feminino; M = Masculino; Não Binário = NB 
 var _gender:String
-var gender:String:
+@export_enum("Masculino", "Feminino", "Não Binário") var gender:String:
 	get:
 		if self._gender.split()[0] in ["M", "MASCULINO", "HOMEM", "MENINO"]:
 			return "e"
@@ -34,47 +44,54 @@ var gender:String:
 	set(gen):
 		self._gender = gen.strip_edges().to_upper()
 var _level:int
-var level:int = 0:
+@export_range(0, 10) var level:int = 0:
 	get: return self._level
 	set(novo): _level = max(0, min(10, novo))
 
 # Atributos de 1 a 20 D&D vibes
-var STR:int = 10
+@export_range(1, 30) var STR:int = 10
 var STR_mod:int = 0
-var DEX:int = 10
+@export_range(1, 30) var DEX:int = 10
 var DEX_mod:int = 0
-var CON:int = 10
+@export_range(1, 30) var CON:int = 10
 var CON_mod:int = 0
-var ESP:int = 10
+@export_range(1, 30) var ESP:int = 10
 var ESP_mod:int = 0
 
 var _hp_max:int
-var hp_max:int:
+@export_range(1, 999) var hp_max:int = 10:
 	get: return self._hp_max
 	set(new): self._hp_max = max(1, new)
 var hp_max_mod:int
 var _hp_current:int
-var hp_current:int:
+@export var hp_current:int = 10:
 	get: return self._hp_current
-	set(new): self._hp_current = min(self.get_max_health(), new)
+	set(new): self._hp_current = max(0, min(self.get_max_health(), new))
 var _mp_max:int
-var mp_max:int:
+@export_range(1, 999) var mp_max:int = 10:
 	get: return self._mp_max
 	set(new): self._mp_max = max(1, new)
 var mp_max_mod:int
 var _mp_current:int
-var mp_current:int:
+@export var mp_current:int = 10:
 	get: return self._mp_current
-	set(new): self._mp_current = min(self.get_max_mana(), new)
+	set(new): self._mp_current = max(0, min(self.get_max_mana(), new))
 
 # Inventário
 
 func attack(ATR_base:String="STR") -> int:
-	return randi_range(1, 10) + atribute_mod(ATR_base)
+	return generic_roll(ATR_base)
+func give_damage(ATR_base:String="STR") -> int:
+	return randi_range(1, 4) + randi_range(1, 4) + max(atribute_mod(ATR_base), 0)
+func dodge() -> int:
+	return generic_roll("DEX", false)
 func defend():
-	return 
+	return atribute_mod("CON")*2 + self.level
 func run():
 	return randi_range(1, 100)
+func take_damage(damage:int) -> void:
+	var temp = self.hp_current - max(damage-self.get_CON(), 0)
+	self.hp_current = temp
 func atribute_mod(attr:String, mod:bool=true):
 	attr.strip_edges().to_upper()
 	var getters:Dictionary[String, Callable] = {
@@ -101,3 +118,11 @@ func get_max_health(mod:bool=true) -> int:
 func get_max_mana(mod:bool=true) -> int:
 	var mp:int = self.mp_max+self.mp_max_mod if mod else self.mp_max
 	return max(1, mp)
+func estoy_muerto() -> bool:
+	return true if self.hp_current<1 else false
+func generic_roll(attr:String, prof_mod:bool=true, attr_mod:bool=true) -> int:
+	var dice:int = randi_range(1, 10)
+	var modifier:int = atribute_mod(attr, attr_mod)
+	if prof_mod:
+		modifier+=level
+	return dice + modifier
